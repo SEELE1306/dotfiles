4badf3888d061636c5bc87089b429cff9e8d6acd
SEELE1306 <mkdang@proton.me>
2025-05-20 01:01:18 +0200

    Add config for fish

---
+++ fish/config.fish
@@ -0,0 +1,69 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Add new directory to PATH
+# echo 'export PATH=$PATH:/home/aoi/.local/bin' >> ~/.config/fish/config.fish
+# export PATH=$PATH:/home/aoi/.local/bin
+
+# Alias's to modified commands
+alias dots='cd ~/dotfiles/'
+alias conf='micro ~/.config/fish/config.fish'
+alias ffconf='micro ~/.config/fastfetch/config.jsonc'
+#alias cd='cd && ls'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='trash -v'
+alias mkdir='mkdir -p'
+alias ps='ps auxf'
+alias ping='ping -c 10'
+alias less='less -R'
+alias multitail='multitail --no-repeat -c'
+#alias src='source ~/.config/fish/config.fish'
+alias ga='git add .'
+alias gcom='git commit -m'
+
+# Alias's for multiple directory listing commands
+alias la='ls -Alh --color=always' # show hidden files
+alias ls='ls -aFh --color=always' # add colors and file type extensions
+alias lx='ls -lXBh'               # sort by extension
+alias lk='ls -lSrh'               # sort by size
+alias lc='ls -ltcrh'              # sort by change time
+alias lu='ls -lturh'              # sort by access time
+alias lr='ls -lRh'                # recursive ls
+alias lt='ls -ltrh'               # sort by date
+alias lm='ls -alh |more'          # pipe through 'more'
+alias lw='ls -xAh'                # wide listing format
+alias ll='ls -Fls'                # long listing format
+alias labc='ls -lap'              # alphabetical sort
+alias lf="ls -l | egrep -v '^d'"  # files only
+alias ldir="ls -l | egrep '^d'"   # directories only
+alias lla='ls -Al'                # List and Hidden Files
+alias las='ls -A'                 # Hidden Files
+alias lls='ls -l'                 # List
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
+
+# Abbreviations
+abbr dnf sudo dnf
+abbr ff fastfetch
+abbr cls clear
+abbr fishconf ~/.config/fish/config.fish
+
+# Default Editor
+set EDITOR micro
+set VISUAL micro
+alias smicro="sudo micro"
+
+# To have colors for ls and all grep commands such as grep, egrep and zgrep
+export CLICOLOR=1
+export LS_COLORS='no=00:fi=00:di=00;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:*.xml=00;31:'
+
+# Start Starship
+starship init fish | source
